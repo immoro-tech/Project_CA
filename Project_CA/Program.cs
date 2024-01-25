87bd5b7761ce694d6282f7e7f7b61306c2cb458b
qpzdon <fedorenkokirill71@gmail.com>
2024-01-25 14:58:19 +0200

    Delete all code

--- Project_CA/Program.cs
+++ Project_CA/Program.cs
@@ -11,39 +11,7 @@ namespace Project_CA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose an action:" +
-                "\n1.Create file" +
-                "\n2.Open file");
-            int actionStatus = Int32.Parse(Console.ReadLine());
-            switch(actionStatus)
-            {
-                case 1:
-                    Functionality.CreateFile();
-                    break;
-                
-                case 2:
-
-                    Functionality.OpenFile();
-                    break;
-            }
-
-            Console.WriteLine("Add or Change dimension:");
-            actionStatus = Int32.Parse(Console.ReadLine());
-
-            switch (actionStatus)
-            {
-                case 1:
-                    FileChanges.AddDimensions();
-                    break;
-                
-                case 2:
-                    FileChanges.ChangesDimensions();
-                    break;
-            }
-
-
-
-            Console.ReadKey();
+            
         }
     }
 }
